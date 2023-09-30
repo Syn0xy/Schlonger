1d590bd012157b775d1109057ab75622b9d82a1e
Synoxy <sinoxap@gmail.com>
2023-09-30 11:35:25 +0200

    feat: le bot peut se connecter et envoyer des messages à une horaire precise

--- index.js
+++ index.js
@@ -1,22 +1,53 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const cron = require('node-cron');
 const fs = require('fs');
 let rawdata = fs.readFileSync('config.json');
 let config = JSON.parse(rawdata);
 
-const token = config.token;
+const TOKEN = config.token;
+const PREFIX = config.prefix;
+const SEPARATOR = " ";
 
-console.log(token);
+const CHANNEL_LOG_ID = '1157601117736275998';
+const CHANNEL_PICTURES_ID = '1157607727544156270';
 
-client.login(token);
+const commande1 = "ping";
 
-// client.on('ready', function () {
-//   console.log("Je suis connecté !");
-// });
+client.login(TOKEN);
 
-// client.on('message', message => {
-// 	if (message.content === 'ping') {
-// 		message.reply('pong !');
-// 	}
-// });
+client.on('ready', () => {
+	log("Je suis connecte !");
+	
+	cron.schedule('* * * * *', () => {
+		sendMessage();
+	});
+});
+
+client.on("message", (msg) => {
+	if (msg.author.id === client.user.id) return;
+	if(msg.content.substring(0, PREFIX.length) !== PREFIX) return;
+	log("author : " + msg.author.username + " , content : " + msg.content);
+	const args = msg.toLowerCase().content.substring(PREFIX.length).split(SEPARATOR);
+	log(args[0]);
+    switch(args[0]){
+        case commande1:
+          log("pong !");
+          msg.reply("pong !");
+            break;
+    }
+});
+
+const sendMessage = () => {
+	const CHANNEL_PICTURES = client.channels.cache.get(CHANNEL_PICTURES_ID);
+	const currentTime = new Date();
+	const msg = "Date:" + currentTime.getDate() + ", heures:" + currentTime.getHours() + ", minutes:" + currentTime.getMinutes() + ", secondes:" + currentTime.getSeconds();
+	CHANNEL_PICTURES.send(msg);
+}
+
+function log(msg){
+	const CHANNEL_LOG = client.channels.cache.get(CHANNEL_LOG_ID);
+	CHANNEL_LOG.send(msg);
+	console.log(msg);
+}
